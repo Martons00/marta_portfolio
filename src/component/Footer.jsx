e6c22bae2d7c897ba4e351ab84f043a00f6a5e2f
Martons00 <raffaelemartone34@gmail.com>
2025-04-14 23:56:23 +0200

    fix del footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './CSS/Footer.css';
 
 function Footer() {
@@ -6,25 +7,49 @@ function Footer() {
     <footer className="site-footer">
       <div className="footer-container">
         <div className="footer-left">
-          <p className="copyright">© {new Date().getFullYear()} Marta Spaccapietra</p>
+          <p className="copyright">
+            © {new Date().getFullYear()} Marta Spaccapietra
+          </p>
         </div>
         
         <div className="footer-center">
           <nav className="footer-nav">
             <ul>
-              <li><a href="/marta_portfolio/work">Work</a></li>
-              <li><a href="/marta_portfolio/about">About</a></li>
-              <li><a href="/marta_portfolio/journal">Journal</a></li>
-              <li><a href="/marta_portfolio/contact">Contact</a></li>
+              <li>
+                <NavLink to="/marta_portfolio/work">
+                  Work
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/marta_portfolio/about">
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/marta_portfolio/journal">
+                  Journal
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/marta_portfolio/contact">
+                  Contact
+                </NavLink>
+              </li>
             </ul>
           </nav>
         </div>
         
         <div className="footer-right">
           <div className="social-links">
-            <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a>
-            <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <a href="https://github.com/" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+              Instagram
+            </a>
+            <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+            <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
           </div>
         </div>
       </div>
